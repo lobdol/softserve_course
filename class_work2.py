f270c95f05484625a1185b8de952ae7991ee8225
Pushynskyy Kostya <test@lobdol.org.ua>
2019-05-02 11:30:29 +0300

    add my task2

---
+++ class_work2.py
@@ -0,0 +1,6 @@
+﻿number1=input("enter number1=")
+number1=int(number1)
+if(number1 % 2):
+    print("odd number")
+else:
+    print('even number') 
---
+++ class_work3.py
@@ -0,0 +1,9 @@
+number1=input("enter number1=")
+number1=int(number1)
+i=1
+b=1
+while number1 > i:
+    i=i+1
+    b=b*i
+    
+print(b)
